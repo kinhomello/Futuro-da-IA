96d1af4ab6bedc278ed22c218379f65e0a721eb3
duardellamhl <jose.ferreira.mello@escola.pr.gov.br>
2025-09-04 11:27:57 -0300

    .

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado"); 
-const lista = []
+const lista = [item1, item2]
+
+const perguntas = [
+{
+responda com sim ou não:
+1. você acredita que a intelig
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+    
+]
